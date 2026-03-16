8da56d97d647ae5b0ef8c4f80dd223886bbea0c1
sundip-kumar <sundip@still88.com>
2026-03-16 02:26:00 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+          remotePatterns: [
+            {
+                      protocol: 'http',
+                      hostname: '3.141.1.117',
+                      port: '',
+                      pathname: '/wp-content/uploads/**',
+            },
+                ],
+    },
+}
+
 module.exports = nextConfig
